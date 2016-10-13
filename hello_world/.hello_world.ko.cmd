b08ee65f6c15f8ccf5637d2c117ca34d99578f33
root <root@dev>
2016-10-13 17:56:13 -0400

    net filter and hidden rootkit modules

---
+++ hello_world/.hello_world.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/gerard/repo/linux_kernel_modules/hello_world/hello_world.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.16.0-4-common/scripts/module-common.lds --build-id  -o /home/gerard/repo/linux_kernel_modules/hello_world/hello_world.ko /home/gerard/repo/linux_kernel_modules/hello_world/hello_world.o /home/gerard/repo/linux_kernel_modules/hello_world/hello_world.mod.o
---
+++ hello_world/.tmp_versions/hello_world.mod
@@ -0,0 +1,2 @@
+/home/gerard/repo/linux_kernel_modules/hello_world/hello_world.ko
+/home/gerard/repo/linux_kernel_modules/hello_world/hello_world.o
