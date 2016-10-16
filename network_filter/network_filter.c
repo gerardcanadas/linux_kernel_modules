49c5ad11ef608d7e4456b8db5cd529cd5e6d6554
Gerard <gerard.canadas@gmail.com>
2016-10-16 20:58:18 +0200

    added info on received and sent packets

--- network_filter/network_filter.c
+++ network_filter/network_filter.c
@@ -3,6 +3,9 @@
 #include <linux/init.h>      // included for __init and __exit macros
 #include <linux/netfilter.h>
 #include <linux/vmalloc.h>
+#include <linux/skbuff.h> 
+#include <linux/ip.h> 
+#include <linux/tcp.h>
 
 //#undef __KERNEL__
 #include <linux/netfilter_ipv4.h>
@@ -12,41 +15,125 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Gerard");
 MODULE_DESCRIPTION("Network packet filter");
 
-static struct nf_hook_ops nfho;   //net filter hook option struct
+/* hook fn structs defined by netfilter.h */
+static struct nf_hook_ops nfho_rcv;   /* hook for received packets */
+static struct nf_hook_ops nfho_trm;    /* hook for transmitted packets */
 
-unsigned int my_hook(unsigned int hooknum,
-    struct sk_buff *skb,
-    const struct net_device *in,
-    const struct net_device *out,
-    int (*okfn)(struct sk_buff *))  {
-    struct sock *sk = skb->sk;
-    printk(KERN_INFO "[GS] => packet detected\n");
+/* hook fn for received packets as defined by netfilter.h */
+static unsigned int nf_rcv_hook_fn(const struct nf_hook_ops *ops,
+                                   struct sk_buff *skb,
+                                   const struct net_device *in,
+                                   const struct net_device *out,
+                                   int (*okfn)(struct sk_buff *))
+{
+    struct iphdr *iph;          /* IPv4 header */
+    struct tcphdr *tcph;        /* TCP header */
+    u16 sport, dport;           /* Source and destination ports */
+    u32 saddr, daddr;           /* Source and destination addresses */
+    unsigned char *user_data;   /* TCP data begin pointer */
+    unsigned char *tail;        /* TCP data end pointer */
+    unsigned char *it;          /* TCP data iterator */
+
+  /* Network packet is empty, seems like some problem occurred. Skip it */
+    if (!skb)
+        return NF_ACCEPT;
+
+    iph = ip_hdr(skb);          /* get IP header */
+
+    tcph = tcp_hdr(skb);        /* get TCP header */
+
+    /* Convert network endianness to host endiannes */
+    saddr = ntohl(iph->saddr);
+    daddr = ntohl(iph->daddr);
+    sport = ntohs(tcph->source);
+    dport = ntohs(tcph->dest);
+
+    printk(KERN_INFO "[GS] received packet info: %pI4h:%d -> %pI4h:%d\n", &saddr, sport, &daddr, dport);
+
     return NF_ACCEPT;
 }
 
-static int init_filter_if(void)
+/* Hook fn for transmitted packets as defined by netfilter.h */
+static unsigned int nf_trm_hook_fn(const struct nf_hook_ops *ops,
+                                   struct sk_buff *skb,
+                                   const struct net_device *in,
+                                   const struct net_device *out,
+                                   int (*okfn)(struct sk_buff *))
 {
-  nfho.hook = my_hook;
-  nfho.hooknum = 0 ; //NF_IP_PRE_ROUTING;
-  nfho.pf = PF_INET;
-  nfho.priority = NF_IP_PRI_FIRST;
+    struct iphdr *iph;          /* IPv4 header */
+    struct tcphdr *tcph;        /* TCP header */
+    u16 sport, dport;           /* Source and destination ports */
+    u32 saddr, daddr;           /* Source and destination addresses */
+    unsigned char *user_data;   /* TCP data begin pointer */
+    unsigned char *tail;        /* TCP data end pointer */
+    unsigned char *it;          /* TCP data iterator */
 
-  nf_register_hook(&nfho);
+  /* Network packet is empty, seems like some problem occurred. Skip it */
+    if (!skb)
+        return NF_ACCEPT;
 
-  return 0;
+    iph = ip_hdr(skb);          /* get IP header */
+
+    tcph = tcp_hdr(skb);        /* get TCP header */
+
+    /* Convert network endianness to host endiannes */
+    saddr = ntohl(iph->saddr);
+    daddr = ntohl(iph->daddr);
+    sport = ntohs(tcph->source);
+    dport = ntohs(tcph->dest);
+
+    printk(KERN_INFO "[GS] transmitted packet info: %pI4h:%d -> %pI4h:%d\n", &saddr, sport, &daddr, dport);
+
+    return NF_ACCEPT;
+
 }
 
+static int init_rcv_filter(void)
+{
+  nfho_rcv.hook = nf_rcv_hook_fn; /* Assign defined hook for received packets */
+  nfho_rcv.hooknum = NF_INET_PRE_ROUTING; /* Received packets */
+  nfho_rcv.pf = PF_INET; /* Hook only IPv4 packets */
+  nfho_rcv.priority = NF_IP_PRI_FIRST; /* Max priority hook */
+
+  int hook_result;
+  hook_result = nf_register_hook(&nfho_rcv);
+  if (hook_result < 0) {
+    printk(KERN_ERR "[GS] cannot register hook for received packets");
+  }
+  return hook_result;
+}
+
+static int init_trm_filter(void)
+{
+  nfho_trm.hook = nf_trm_hook_fn; /* Assign defined hook for tramsitted packets */
+  nfho_trm.hooknum = NF_INET_POST_ROUTING; /* Transmitted packets */
+  nfho_trm.pf = PF_INET; /* Hook only IPv4 packets */
+  nfho_trm.priority = NF_IP_PRI_FIRST; /* Max priority hook */
+
+  int hook_result;
+  hook_result = nf_register_hook(&nfho_trm);
+  if (hook_result < 0) {
+    printk(KERN_ERR "[GS] cannot register hook for transmitted packets");
+  }
+  return hook_result;
+}
+
+/* Module init and cleanup */
 int __init netfilter_init(void)
 {
-    printk(KERN_INFO "Hello world!\n");
-    init_filter_if();
-    return 0;    // Non-zero return means that the module couldn't be loaded.
+    printk(KERN_INFO "[GS] network filter module startup.\n");
+    init_rcv_filter();
+    init_trm_filter();
+    printk(KERN_INFO "[GS] rcv and trm hooks registered, startup completed");
+    return 0;    /* Non-zero return for unsuccessfully module startup */ 
 }
 
 void __exit netfilter_cleanup(void)
 {
-  nf_unregister_hook(&nfho);
-  printk(KERN_INFO "[GS] Cleaning up netfilter module.\n");
+  printk(KERN_INFO "[GS] finishing netfilter module, starting cleanup");
+  nf_unregister_hook(&nfho_rcv);
+  //nf_unregister_hook(&nfho_trm);
+  printk(KERN_INFO "[GS] hooks unregistered, cleanup completed.\n");
 }
 
 module_init(netfilter_init);
